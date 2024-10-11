adf1ac58f0eb68307cca8a4265bed023534015e3
MohaK <mohakhalili@outlook.com>
2024-10-11 17:22:56 +0200

    finished user cards

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,23 @@ export default function () {
   }, []);
   console.log(users);
   return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>{user.name}</li>
-      ))}
-    </ul>
+    <div className="m-8">
+      <h1 className="text-3xl ">Användare</h1>
+      <ul className="grid grid-cols-1 laptop:grid-cols-4 gap-8 justify-items-center mt-4">
+        {users.map((user) => (
+          <li className="w-full p-4 shadow rounded" key={user.id}>
+            <div className="flex items-center">
+              <img className="w-12 h-12 rounded-full mr-4" src={user.avatar} />
+              <div className="flex flex-col">
+                <p className="font-semibold">{user.email}</p>
+                <p className="text-gray-500"> {user.name}</p>
+                <p>Phone {user.phone}</p>
+              </div>
+              <div className="flex justify-between"></div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
