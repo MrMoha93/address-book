771ff8b7f05779c1ba7a106289bdb38f41146ab5
MohaK <mohakhalili@outlook.com>
2025-03-08 15:24:45 +0100

    installed fontawesome for icons

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 import UserProvider from "./context/UserContext";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
