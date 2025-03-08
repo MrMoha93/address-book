0e54109ef7afa5c1c4e4e6fc89ab5c1912425834
MohaK <mohakhalili@outlook.com>
2025-03-08 22:34:20 +0100

    added daisyui and implemented light/dark mode

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -3,7 +3,11 @@ import { UserProps } from "../types";
 
 export default function User({ user }: UserProps) {
   return (
-    <li className="w-full p-4 shadow rounded cursor-pointer hover:scale-105 transition-transform duration-300 ">
+    <li
+      className="w-full p-4 shadow rounded cursor-pointer hover:scale-105 transition-transform duration-300
+      bg-white dark:bg-gray-800 
+      text-gray-900 dark:text-gray-100"
+    >
       <Link to={`/${user.id}`} className="flex items-center">
         <img
           className="w-12 h-12 rounded-full mr-4"
@@ -12,10 +16,9 @@ export default function User({ user }: UserProps) {
         />
         <div className="flex flex-col">
           <p className="font-semibold">{user.email}</p>
-          <p className="text-gray-500">{user.name}</p>
+          <p className="text-gray-500 dark:text-gray-400">{user.name}</p>
           <p>Phone {user.phone}</p>
         </div>
-        <div className="flex justify-between"></div>
       </Link>
     </li>
   );
